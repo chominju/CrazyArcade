65ba7f6953fb874c372d8f2c98c594fcae8cd6ab
chominju <colorday3908@gmail.com>
2021-07-02 20:56:12 +0900

    ???

--- CrazyArcade/MFCTool/Resource.h
+++ CrazyArcade/MFCTool/Resource.h
@@ -8,23 +8,39 @@
 #define IDR_MFCToolTYPE                 130
 #define IDD_FORM                        310
 #define IDD_MAPTOOL                     311
+#define IDD_ObjectTool                  312
 #define IDC_BUTTON1                     1000
+#define IDC_BUTTON6                     1001
 #define IDC_LIST1                       1002
 #define IDC_BUTTON2                     1003
 #define IDC_PICTURE                     1004
+#define IDC_BUTTON7                     1004
 #define IDC_BUTTON3                     1005
 #define IDC_LIST2                       1007
+#define IDC_COMBO2                      1007
 #define IDC_BUTTON4                     1008
 #define IDC_BUTTON5                     1009
+#define IDC_EDIT1                       1009
 #define IDC_LIST3                       1010
+#define IDC_EDIT2                       1011
+#define IDC_RADIO1                      1012
+#define IDC_RADIO2                      1013
+#define IDC_RADIO3                      1014
+#define IDC_RADIO4                      1015
+#define IDC_RADIO5                      1016
+#define IDC_RADIO6                      1017
+#define IDC_RADIO7                      1018
+#define IDC_RADIO8                      1019
+#define IDC_RADIO9                      1020
+#define IDC_RADIO10                     1021
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        312
+#define _APS_NEXT_RESOURCE_VALUE        314
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1006
-#define _APS_NEXT_SYMED_VALUE           312
+#define _APS_NEXT_CONTROL_VALUE         1017
+#define _APS_NEXT_SYMED_VALUE           313
 #endif
 #endif
