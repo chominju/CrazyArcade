42ed135020fd9287875edc18a3423f3e307e7b6b
min ju cho <colorday3908@naver.com>
2021-07-02 14:53:43 +0900

    맵짜기

--- CrazyArcade/Headers/Extern.h
+++ CrazyArcade/Headers/Extern.h
@@ -3,7 +3,7 @@
 extern HWND g_hwnd;
 
 const DWORD WINCX = 1200;
-const DWORD WINCY = 1024;
+const DWORD WINCY = 900;
 const DWORD TILECX = 40;
 const DWORD TILECY = 40;
 const DWORD BOXCX = 120;
@@ -11,4 +11,6 @@ const DWORD BOXCY = 160;
 const DWORD WALLCX = 40;
 const DWORD WALLCY = 40;
 const DWORD TILEX = 15;
-const DWORD TILEY = 15;
+const DWORD TILEY = 13;
+
+const float expansionSize = 1.5;
